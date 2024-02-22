96a7731350c4c3724eae1d3a969d020b0267b5c9
userdocs <16525024+userdocs@users.noreply.github.com>
2024-02-22 21:52:36 +0000

    test

---
+++ docs/.docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("I:\\github\\yme\\qbt-static\\node_modules\\infima\\dist\\css\\default\\default.css"),
+  require("I:\\github\\yme\\qbt-static\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
+  require("I:\\github\\yme\\qbt-static\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
+  require("I:\\github\\yme\\qbt-static\\src\\css\\custom.css"),
+];
